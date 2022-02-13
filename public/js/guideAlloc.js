3573c1be6f66544de322307d43483596a3a97045
AnushkaM02 <anushka.mulik@gmail.com>
2022-02-13 13:09:38 +0530

    preference design

--- public/js/guideAlloc.js
+++ public/js/guideAlloc.js
@@ -1,4 +1,4 @@
-document.querySelector('#book').addEventListener('click',async ()=>{
+document.querySelector('#book').addEventListener('click', async () => {
     const pincode = 370201;
     const res = await axios({
         method: "GET",
@@ -7,6 +7,6 @@ document.querySelector('#book').addEventListener('click',async ()=>{
     })
     document.querySelector("#name").innerHTML = `Name: ${res.data.guide.fname} ${res.data.guide.lname}`;
     document.querySelector("#contact").innerHTML = `Phone no.: ${res.data.guide.contact}`;
-    document.querySelector("#description").innerHTML = `Description: ${res.data.guide.description}`;
+    document.querySelector("#description").innerHTML = `${res.data.guide.description}`;
     document.querySelector("#allocation").style = "display :block !important"
 })
